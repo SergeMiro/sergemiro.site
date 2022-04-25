3deebb1a5996ef68b52041816f1e632070092ee7
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-04-25 12:39:53 +0200

    Changement homeMob image

--- js/testScript.js
+++ js/testScript.js
@@ -108,76 +108,138 @@
 // console.log(logWithLastName('Miro'))
 
 
-//Массивы
-const cars = ['Mazda', 'Ford', 'BMW', 'Mersedez']
-const fib = [1, 1, 2, 3, 5, 8, 13]
+// //Массивы - Array
+// const cars = ['Mazda', 'Ford', 'BMW', 'Mersedez']
+// const fib = [1, 1, 2, 3, 5, 8, 13]
 
-function addItemToEnd() {
+// function addItemToEnd() {
+// }
+
+// //Метод для массива
+// cars.push('Renault')
+// cars.unshift('Volga')
+// const	firstItem = cars.shift()
+// console.log(firstItem);
+// console.log(cars)
+
+// //Задача-1
+// const text = 'Hello, life is good'
+// const reverseText = text.split('').reverse().join('')
+// console.log(reverseText);
+
+// //----------
+// const index = cars.indexOf('Ford')
+// console.log(cars[index])
+
+// //---------
+// const people = [
+// 	{name: 'Serge', budget: 1500},
+// 	{name: 'ALina', budget: 1000},
+// 	{name: 'Richi', budget: 2000}
+// ]
+// const person = people.find(function(person){
+// 	return person.budget === 1500
+// })
+// console.log(person)
+
+// //---------
+// let findedPerson
+// for (const person of people) {
+// 	if (person.budget === 1000) {
+// 		findedPerson = person
+// 	}
+// }
+// console.log(findedPerson);
+
+// //Методы высокого порядка:
+// //--Метод MAP, возврат массива
+// const upperCaseCars = cars.map (car => {
+// 	return car.toUpperCase()
+// })
+// console.log(upperCaseCars)
+
+// const pow2Fib = fib.map (num => num **2 )
+// console.log(pow2Fib);
+
+// const pow3 = num =>num **2
+// const pow3Fib = fib.map(pow3)
+// console.log(pow3Fib);
+
+// //--Метод Filter, фильтрация массива
+// const filteredNumbers = pow3Fib.filter(num=> {
+// 	return num>20
+// })
+// console.log (filteredNumbers);
+
+// //--Метод Reduce, соединение данных
+// const allBudget = people
+// 	.filter (person => person.budget>1000)
+// 	.reduce((acc, person) => {
+// 		acc += person.budget
+// 		return acc
+// 	}, 0)
+// console.log(allBudget)
+
+
+// Обьекты - objects
+const person = {
+	name: 'Serge',
+	lastName: 'Miro',
+	age: 31,
+	isProgrammer: true,
+	languages: ['ru', 'en', 'fr'],
+	//синтаксис для добавления ключа в виде функции в обьект
+	greet() {console.log('greet from person')},
+	//синтаксис для добавления ключа в кавычках в обьект, для сложных названий
+	'complex key': 'Complex Value',
+	['key_'+(4+2)]: 'Computed Key', //key_6
 }
+console.log(person.name)
+console.log(person['age'])
 
-//Метод для массива
-cars.push('Renault')
-cars.unshift('Volga')
-const	firstItem = cars.shift()
-console.log(firstItem);
-console.log(cars)
+const ageKey = 'age'
+console.log(person[ageKey])
 
-//Задача-1
-const text = 'Hello, life is good'
-const reverseText = text.split('').reverse().join('')
-console.log(reverseText);
-
-//----------
-const index = cars.indexOf('Ford')
-console.log(cars[index])
-
-//---------
-const people = [
-	{name: 'Serge', budget: 1500},
-	{name: 'ALina', budget: 1000},
-	{name: 'Richi', budget: 2000}
-]
-const person = people.find(function(person){
-	return person.budget === 1500
-})
+console.log(person['complex key'])
 console.log(person)
+person.greet()
 
-//---------
-let findedPerson
-for (const person of people) {
-	if (person.budget === 1000) {
-		findedPerson = person
-	}
+person.age++ 
+person.languages.push('ukr')
+
+delete person['key_6']
+console.log(person);
+
+// //**********************************************
+// //Деструктуризация - преобразовать ключи обьекта в переменные CONST
+// //Пример:
+// const name = person.name
+// const age = person.age
+// const languages = person.languages
+// //результат:
+// 	//-1
+// 	const {name, age, languages} = person
+// 	//-2
+// 	const {name, age: personAge=18, languages} = person
+// 	console.log(name);
+// //**********************************************
+
+//Управление обьектом и его ключами через OBJECTS
+Object.keys(person).forEach((key) =>{
+	console.log(key);
+	console.log(person[key]);
+})
+
+//КОНТЕКСТ - Context : (THIS) (withParams)
+withParams (top = false, between = false, bottom = false) {
+	if (top) {console.log(-------start--------)}
+	Object.keys(this).forEach((key, index, array) =>{
+	console.log(`'${key}': ${this[key]}`)
+		if (between && index !==array.length - 1) {
+		console.log('-------------------')
+		}
+	})
+	if (bottom) {console.log('-----------End------------')}
 }
-console.log(findedPerson);
+logger.withParams.call(person, true, true, true)
 
-//Методы высокого порядка:
-//--Метод MAP, возврат массива
-const upperCaseCars = cars.map (car => {
-	return car.toUpperCase()
-})
-console.log(upperCaseCars)
-
-const pow2Fib = fib.map (num => num **2 )
-console.log(pow2Fib);
-
-const pow3 = num =>num **2
-const pow3Fib = fib.map(pow3)
-console.log(pow3Fib);
-
-//--Метод Filter, фильтрация массива
-const filteredNumbers = pow3Fib.filter(num=> {
-	return num>20
-})
-console.log (filteredNumbers);
-
-//--Метод Reduce, соединение данных
-const allBudget = people
-	.filter (person => person.budget>1000)
-	.reduce((acc, person) => {
-		acc += person.budget
-		return acc
-	}, 0)
-console.log(allBudget)
-
-//
