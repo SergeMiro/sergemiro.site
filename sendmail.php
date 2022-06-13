44a8cc25c934628366f7dc32fc14095fd7a96a7e
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-06-13 11:51:47 +0200

    Ajout et config php.mailer

---
+++ sendmail.php
@@ -0,0 +1,60 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('mirochnyk204628@gmail.com', 'SergeMiro Dev');
+	//Кому отправить
+	$mail->addAddress('mirochnyk.sergiy@gmail.com!');
+	//Тема письма
+	$mail->Subject = 'SALUT, UN DEVIS POUR TOI - SergeMiro Dev -';
+
+
+
+	//Тело письма
+	$body = '<h1>Voici le message :</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Message:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.="<p><strong>Capture d'écran :</strong>";
+			$mail->addAttachment($fileAttach);
+		}
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Erreur';
+	} else {
+		$message = 'Votre message a été envoyé.';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
